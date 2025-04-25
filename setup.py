18344eebb243addb7375ab22ed6293d50d667f72
Amidu Kamara <midofemi@yahoo.com>
2025-04-24 23:58:17 -0400

    Setup file updated

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-def get_requirements()->List[str]:
-    """
-    This function will return list of requirements
-    """
-    requirement_list:List[str] = []
-
-    """
-    Write a code to read requirements.txt file and append each requirements in requirement_list variable.
-    """
-    return requirement_list
+def get_requirements(file_path: str = "requirements.txt") -> List[str]:
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [r.strip() for r in requirements if r.strip() and not r.startswith("#")]
+        return requirements
 
 setup(
     name="sensor",
     version="0.0.1",
     author="Amidu Kamara",
     author_email="midofemi@yahoo.com",
-    packages = find_packages(),
-    install_requires=get_requirements(),#["pymongo==4.2.0"],
+    packages=find_packages(),
+    install_requires=get_requirements(),
 )
